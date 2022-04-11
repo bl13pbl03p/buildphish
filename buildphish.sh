0395d3e26ca4f8950fa036ee614d44b67423ce16
bl13bl03p <22095577+bl13pbl03p@users.noreply.github.com>
2022-04-11 13:56:29 +0200

    Update buildphish.sh

--- buildphish.sh
+++ buildphish.sh
@@ -21,4 +21,4 @@ echo "[?] Check if service is running: sudo systemctl status gophish"
 echo "[?] If service does not run, troubleshoot the /etc/systemd/system/gophish.service file"
 echo "[*] Visit https://localhost:3333 to login"
 echo "[*] Execute the script beneath to retrieve the initial username and password"
-echo "cat /var/log/gophish/gophish.log | grep 'Please login with the username admin and the password'
+echo "cat /var/log/gophish/gophish.log | grep 'Please login with the username admin and the password'"
