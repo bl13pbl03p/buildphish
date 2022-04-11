66b58a055f41116c7e033ce8bab6660c5060efbf
bl13bl03p <22095577+bl13pbl03p@users.noreply.github.com>
2022-04-11 15:16:52 +0200

    Update buildphish.sh

--- buildphish.sh
+++ buildphish.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 FILE=/etc/systemd/system/gophish.service
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]; 
+then
 	echo' 
-	    ____        _ __    ______  __    _      __  
+		____        _ __    ______  __    _      __  
 	   / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
 	  / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
 	 / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
 	/_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
 	Made by bl13pbl03p                          v.0.1'
-	  
-    echo "[-] Gophish is already configured as a service"
-    echo "[-] Terminating BuildPhish"
+
+	echo "[-] Gophish is already configured as a service"
+	echo "[-] Terminating BuildPhish"
 else 
 	echo' 
-	    ____        _ __    ______  __    _      __  
+		____        _ __    ______  __    _      __  
 	   / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
 	  / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
 	 / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
 	/_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
 	Made by bl13pbl03p                          v.0.1'
-        
+
 	# Installing Gophish
 	echo "[+] Let's build!"                                                 
 	sudo service apache2 stop
