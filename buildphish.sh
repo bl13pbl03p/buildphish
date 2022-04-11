cb282cc29549364c36545e5386357b22b194c412
bl13bl03p <22095577+bl13pbl03p@users.noreply.github.com>
2022-04-11 15:04:03 +0200

    Update buildphish.sh

--- buildphish.sh
+++ buildphish.sh
@@ -2,24 +2,24 @@
 
 FILE=/etc/systemd/system/gophish.service
 if [ -f "$FILE" ]; then
-    echo' 
-	      ____        _ __    ______  __    _      __  
-	     / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
-	    / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
-	   / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
-	  /_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
-      Made by bl13pbl03p                          v.0.1'
+	echo' 
+	    ____        _ __    ______  __    _      __  
+	   / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
+	  / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
+	 / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
+	/_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
+	Made by bl13pbl03p                          v.0.1'
 	  
     echo "[-] Gophish is already configured as a service"
     echo "[-] Terminating BuildPhish"
 else 
 	echo' 
-	      ____        _ __    ______  __    _      __  
-	     / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
-	    / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
-	   / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
-	  /_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
-	  Made by bl13pbl03p                          v.0.1'
+	    ____        _ __    ______  __    _      __  
+	   / __ )__  __(_) /___/ / __ \/ /_  (_)____/ /_ 
+	  / __  / / / / / / __  / /_/ / __ \/ / ___/ __ \
+	 / /_/ / /_/ / / / /_/ / ____/ / / / (__  ) / / /
+	/_____/\__,_/_/_/\__,_/_/   /_/ /_/_/____/_/ /_/
+	Made by bl13pbl03p                          v.0.1'
         
 	# Installing Gophish
 	echo "[+] Let's build!"                                                 
@@ -44,7 +44,7 @@ else
 	echo "[+] Gophish is setup as service"
 	echo "[?] Check if service is running: sudo systemctl status gophish"
 	echo "[?] If service does not run, troubleshoot the /etc/systemd/system/gophish.service file"
+	echo "[*] Username is admin"
+	echo "[*] View initial password with: cat /var/log/gophish/gophish.log | grep 'Please login with the username admin and the password'"
 	echo "[*] Visit https://localhost:3333 to login"
-	echo "[*] Username is admin, password is shown below:"
-	cat /var/log/gophish/gophish.log | grep 'Please login with the username admin and the password'
 fi
