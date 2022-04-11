cb2fadbe219f6328553b6754cc4a9b7280a90b14
bl13bl03p <22095577+bl13pbl03p@users.noreply.github.com>
2022-04-11 13:51:00 +0200

    Update buildphish.sh

--- buildphish.sh
+++ buildphish.sh
@@ -1,6 +1,7 @@
 # Installment
 sudo service apache2 stop
 echo "[+] Stoppped apache2"
+echo "[+] Downloading Gophish"
 wget https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
 sudo unzip gophish-v0.11.0-linux-64bit.zip -d /opt/gophish
 sudo chmod +x /opt/gophish/gophish
@@ -8,34 +9,16 @@ echo "[+] Installed gophish"
 
 # Setting up as service
 sudo useradd -r gophish
-sudo cat << EOF >> /etc/systemd/system/gophish.service
-[Unit]
-Description=Gophish is an open-source phishing toolkit
-Documentation=https://getgophish.com/documentation/
-After=network.target
-
-[Service]
-WorkingDirectory=/opt/gophish
-User=gophish
-Environment='STDOUT=/var/log/gophish/gophish.log'
-Environment='STDERR=/var/log/gophish/gophish.log'
-PIDFile=/var/run/gophish
-ExecStart=/bin/sh -c '/opt/gophish/gophish >>${STDOUT} 2>>${STDERR}'
-
-[Install]
-WantedBy=multi-user.target
-Alias=gophish.service
-EOF
-
+sudo mv gophish.service /etc/systemd/system/
 sudo mkdir /var/log/gophish
 sudo chown -R gophish:gophish /opt/gophish/ /var/log/gophish/
 sudo setcap cap_net_bind_service=+ep /opt/gophish/gophish
 sudo systemctl daemon-reload
 sudo systemctl start gophish
 sudo systemctl enable gophish
-sudo systemctl status gophish
 echo "[+] Gophish is setup as service"
-echo "[?] If service does not run, check the /etc/systemd/system/gophish.service"
-echo "[?] This must be include in the file above: /opt/gophish/gophish >>${STDOUT} 2>>${STDERR}"
-echo "[*] Execute the script beneath to retrieve the initial password"
+echo "[?] Check if service is running: sudo systemctl status gophish"
+echo "[?] If service does not run, troubleshoot the /etc/systemd/system/gophish.service file"
+echo "[*] Visit https://localhost:3333 to login"
+echo "[*] Execute the script beneath to retrieve the initial username and password"
 echo "cat /var/log/gophish/gophish.log | grep 'Please login with the username admin and the password'
