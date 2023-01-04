179d3898ee95764f58ba95d33b572357fc7a18c5
bl13bl03p <22095577+bl13pbl03p@users.noreply.github.com>
2023-01-04 16:58:47 +0100

    Update buildphish.sh
    show gophish password

--- buildphish.sh
+++ buildphish.sh
@@ -76,6 +76,6 @@ sudo systemctl enable gophish
 echo -e "\033[32m[+] Gophish is setup as service\033[0m"
 echo -e "\033[33m[?] Check if service is running: sudo systemctl status gophish\033[0m"
 echo -e "\033[33m[?] If service does not run, troubleshoot the /etc/systemd/system/gophish.service file\033[0m"
-echo "[*] Username is admin, password is " password
+echo "[*] Username is admin, password is" $password
 echo "[*] Visit https://localhost:3333 to login"
 fi
